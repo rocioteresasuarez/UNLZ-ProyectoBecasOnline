bedf2b947beb05feb38838066cb47e9e156e3cc4
pabloaleramirez <pabloaleramirez@gmail.com>
2018-10-25 01:02:50 -0300

    version 2 de codigo

---
+++ app/assets/js/sign-up/sing-up.js
@@ -0,0 +1,14 @@
+// Notification JavaScripts
+
+(function ($) {
+	'use strict';
+
+
+
+	$('.swal-success').on('click', function () {
+		swal("Felicitaciones!", "Has sido registrado!", "success")
+	});
+
+
+
+})(jQuery);
