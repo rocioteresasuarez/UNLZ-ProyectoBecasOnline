dbaed98339fe76b8d026991069e69d2925f8f71b
pabloaleramirez <pabloaleramirez@gmail.com>
2018-10-27 00:05:17 -0300

    se agregan nuevos html companies-for-user index_jubde my-postgrades postgrades-for-users profile universities-for-user user-more-data

---
+++ app/assets/js/apps/more-data.js
@@ -0,0 +1,14 @@
+// Notification JavaScripts
+
+(function ($) {
+	'use strict';
+
+
+
+	$('.swal-success').on('click', function () {
+		swal("Carga Exitosa!", "Se completaron los Datos Personales!", "success")
+	});
+
+
+
+})(jQuery);
