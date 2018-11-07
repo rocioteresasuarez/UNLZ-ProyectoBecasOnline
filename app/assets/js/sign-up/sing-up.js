d1427f7bb4e1f4b4eb88cae42ec35210292cd743
rocio.teresa.suarez <rocio.teresa.suarez@gmail.com>
2018-11-06 23:15:05 -0300

    Notification JavaScripts

--- app/assets/js/sign-up/sing-up.js
+++ app/assets/js/sign-up/sing-up.js
@@ -3,11 +3,27 @@
 (function ($) {
 	'use strict';
 
-
+	$.validator.messages.required = 'Este campo es obligatorio';
+	$.validator.messages.email = 'Debe ingresar un mail valido.';
 
 	$('.swal-success').on('click', function () {
-		swal("Felicitaciones!", "Has sido registrado!", "success")
+
+
 	});
+
+	$('.swal-cancel').on('click', function () {
+		window.location.href = "index.html";
+	});
+
+	$.validator.setDefaults({
+		submitHandler: function () {
+			swal("Felicitaciones!", "Has sido registrado!", "success")
+		}
+	});
+
+
+	$("#form-validation").validate();
+
 
 
 
